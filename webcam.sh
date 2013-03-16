528d634bcf70c97971f3795579d3ad42b1543aab
Benjie Gillam <benjie@jemjie.com>
2013-03-16 16:53:08 +0000

    Fix webcam script.

--- webcam.sh
+++ webcam.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
-FILE="/tmp/shot-`date +%F.%H-%M-%S`.jpg"
+FILENAME="shot-`date +%F.%H-%M-%S`.jpg"
+FILE="/tmp/$FILENAME"
 fswebcam $FILE
-scp $FILE members.somakeit.org.uk:www
-echo "Webcam: http://irccat.somakeit.org.uk/$FILE"
+scp --preserve $FILE members.somakeit.org.uk:www
+echo "Webcam: http://irccat.somakeit.org.uk/$FILENAME"

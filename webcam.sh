cd50bf3e9666d5603df7903639be6130c92eb184
Benjie Gillam <benjie@jemjie.com>
2013-03-16 17:30:11 +0000

    Debugging

--- webcam.sh
+++ webcam.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 FILENAME="shot-`date +%F.%H-%M-%S`.jpg"
 FILE="/tmp/$FILENAME"
-fswebcam $FILE
-scp --preserve $FILE members.somakeit.org.uk:www
+/usr/bin/fswebcam $FILE 2>&1
+/usr/bin/scp --preserve $FILE members.somakeit.org.uk:www 2>&1
 echo "Webcam: http://irccat.somakeit.org.uk/$FILENAME"

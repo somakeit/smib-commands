8e4ec20563747909fa4d21338d6eae78139f5e24
Benjie Gillam <benjie@jemjie.com>
2013-03-17 17:04:29 +0000

    Remove debugging.

--- webcam.sh
+++ webcam.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 FILENAME="shot-`date +%F.%H-%M-%S`.jpg"
 FILE="/tmp/$FILENAME"
-/usr/bin/fswebcam $FILE 2>&1
+/usr/bin/fswebcam $FILE
 if [ "$?" != "0" ]; then exit 1; fi
-/usr/bin/scp --preserve $FILE members.somakeit.org.uk:www 2>&1
+/usr/bin/scp --preserve $FILE members.somakeit.org.uk:www
 if [ "$?" != "0" ]; then exit 1; fi
 echo "Webcam: http://irccat.somakeit.org.uk/$FILENAME"

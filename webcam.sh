1712b8dcb93432427ac31aa0c4a55c3d2f822131
Benjie Gillam <benjie@jemjie.com>
2013-03-17 16:22:55 +0000

    Error handling.

--- webcam.sh
+++ webcam.sh
@@ -2,5 +2,7 @@
 FILENAME="shot-`date +%F.%H-%M-%S`.jpg"
 FILE="/tmp/$FILENAME"
 /usr/bin/fswebcam $FILE 2>&1
+if [ "$?" != "0" ]; then exit 1; fi
 /usr/bin/scp --preserve $FILE members.somakeit.org.uk:www 2>&1
+if [ "$?" != "0" ]; then exit 1; fi
 echo "Webcam: http://irccat.somakeit.org.uk/$FILENAME"

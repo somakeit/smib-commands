fc82887f6f2058022d4d82c1856648dbd8a0bc8c
Benjie Gillam <benjie@jemjie.com>
2013-03-21 19:51:45 +0000

    Set webcam resolution.

--- webcam.sh
+++ webcam.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 FILENAME="shot-`date +%F.%H-%M-%S`.jpg"
 FILE="/tmp/$FILENAME"
-/usr/bin/fswebcam $FILE
+/usr/bin/fswebcam -r 640x480 --title SoMakeIt $FILE
 if [ "$?" != "0" ]; then exit 1; fi
 /usr/bin/scp --preserve $FILE members.somakeit.org.uk:www
 if [ "$?" != "0" ]; then exit 1; fi

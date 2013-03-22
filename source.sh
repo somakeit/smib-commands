127da00851316abee0f0e9c09dc85045f9ef8100
Benjie Gillam <benjie@jemjie.com>
2013-03-22 15:23:52 +0000

    Link to command source.

---
+++ source.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+if [ "$4" == "" ]; then
+  echo "$1, ?source <commandname>"
+  exit;
+fi
+CMD="$(ls "$4".* 2>/dev/null | head -n 1)"
+if [ "$CMD" == "" ]; then
+  echo "$1, command '$4' not found"
+  exit;
+fi
+echo "https://github.com/so-make-it/irccat-commands/blob/master/$CMD"

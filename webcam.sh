e0664e959c21cf18e68077b8153213b3ea509ba9
Benjie Gillam <benjie@jemjie.com>
2013-03-23 00:03:40 +0000

    Webcam: don't set resulution, skip 3 frames.

--- webcam.sh
+++ webcam.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 FILENAME="shot-`date +%F.%H-%M-%S`.jpg"
 FILE="/tmp/$FILENAME"
-/usr/bin/fswebcam -r 640x480 --title SoMakeIt $FILE
+/usr/bin/fswebcam -S 3 --title SoMakeIt $FILE
 if [ "$?" != "0" ]; then exit 1; fi
 /usr/bin/scp --preserve $FILE members.somakeit.org.uk:www
 if [ "$?" != "0" ]; then exit 1; fi

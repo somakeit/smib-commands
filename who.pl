71119d7f21759099ff3b986809c1ef543ad2289e
Bracken Dawson <abdawson@gmail.com>
2013-04-13 22:43:23 +0100

    Let users register themselvs
    who/this just forces the needed directory to exist, the file
    inside is .gitignored.

--- who.pl
+++ who.pl
@@ -4,18 +4,25 @@ use strict;
 use warnings;
 
 my $nick = shift;
+my $context = shift; # will be "null" if this is a private message, ie registraton
+my $channel = shift; #?
+my $argv = shift;
 
 # This script needs the irccat user to have
 # key auth to root on the dd-wrt router.
 
 # Put your name and sha256 sum of your device's WiFi MAC in the hashref below.
 # To get the sha256 sum of your WiFi mac, do this on a *nix box:
-#   $ echo -n a1:b2:c3:d4:e5:f6 | sha256sum
-my $FRIENDS = {
+#   $ echo -n A1:B2:C3:D4:E5:F6 | sha256sum
+# It MUST be uppercase
+# If you don't want a vanity username, you can add yourself over IRC by typing:
+#   /msg smib A1:B2:C3:d4:e5:f6
+my $friends = {
   '7c94e41c60b497117f00b98e7846476d968f159b061d579e3679474b6073d007' => 'Bracken',
   'b8762da1eafaf929a0e34f3729135ec123f4ee07c842216e8c03e23bbb6e1faf' => 'Bracken',
   '5a226259614fed4be4d7dca236a7d4a42b96a679b0926a0a2f2fdff5059c1b97' => 'LIAR'};
 # If you ever come to me claiming you've cracked this, I expect to know LIAR's MAC.
+my $FRIENDSFILE = 'who/friends.txt';
 
 # uniq
 sub uniq {my %s; grep {!$s{$_}++} @_}
@@ -23,6 +30,38 @@ sub uniq {my %s; grep {!$s{$_}++} @_}
 # ====
 # MAIN
 # ====
+
+#if this is a message, store the hash and bail
+if ($context eq 'null') {
+  my $mac;
+  if ($argv =~ m/([0-9A-Fa-f]{2}:[0-9A-Fa-f]{2}:[0-9A-Fa-f]{2}:[0-9A-Fa-f]{2}:[0-9A-Fa-f]{2}:[0-9A-Fa-f]{2})/) {
+    #user gave us a MAC
+    $mac = $1;
+  }
+  # make it uppercase
+  $mac =~ tr/[a-f]/[A-F]/;
+  # sum it
+  my $sum = `/bin/echo -n $mac | /usr/bin/sha256sum`;
+  if ($sum =~ m/(\w+)/) {
+    $sum = $1; 
+  }
+  #store the sum
+  open FH, ">>$FRIENDSFILE";
+  print FH "$sum $nick\n";
+  close FH;
+  exit(0);
+}
+
+# load more keys into the hashref from the file
+# lines in the file have the format:
+# 234jlkj2349023jlkj234 brackendawson
+open FH, "$FRINDSFILE";
+for my $line (<FH>) {
+  if ($line =~ m/^(\w{64}) (\w+)/) {
+    $friends->{$1} = $2;
+  }
+}
+close FH;
 
 # Get MAC addresses
 my @sums = `cat /tmp/irccat.who.macs.* 2>/dev/null`;

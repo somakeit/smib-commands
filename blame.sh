95319f26870db8d7246b36402276c5f25268b198
Anton Piatek <anton@piatek.co.uk>
2013-04-29 09:23:13 +0200

    echo, not ehco

--- blame.sh
+++ blame.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 
 if [ "$4" == "" ]; then
-  ehco "$1, ?source <commandname>"
+  echo "$1, ?source <commandname>"
   exit;
 fi
 CMD="$(ls "$4".* 2>/dev/null | head -n 1)"
 if [ "$CMD" == "" ]; then
-  ehco "$1, command '$4' not found"
+  echo "$1, command '$4' not found"
   exit;
 fi
-ehco "https://github.com/so-make-it/irccat-commands/blame/master/$CMD"
+echo "https://github.com/so-make-it/irccat-commands/blame/master/$CMD"

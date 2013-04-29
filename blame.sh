e8905eff299fc76e7ec6f5b443be22f77f33f535
Benjie Gillam <benjie@jemjie.com>
2013-04-29 15:01:06 +0100

    Make the blame command do something useful.

--- blame.sh
+++ blame.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 if [ "$4" == "" ]; then
-  echo "$1, ?source <commandname>"
+  echo "$1, ?blame <commandname>"
   exit;
 fi
 CMD="$(ls "$4".* 2>/dev/null | head -n 1)"
@@ -9,4 +9,4 @@ if [ "$CMD" == "" ]; then
   echo "$1, command '$4' not found"
   exit;
 fi
-echo "https://github.com/so-make-it/irccat-commands/blame/master/$CMD"
+echo "$1, the last person to modify the '$4' command was "$(git log -n 1 --pretty=format:"%an https://github.com/so-make-it/irccat-commands/commit/%h" -- "$CMD")

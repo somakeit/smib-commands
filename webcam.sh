a1d2a14e2dad6c2a4c7d5abe00fec8aa74903b29
Bracken Dawson <abdawson@gmail.com>
2013-05-18 17:48:23 +0100

    Still nothing
    No need to sleep, festival is slow enough.

--- webcam.sh
+++ webcam.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
-echo "beep 5" | festival --tts; sleep 1
-echo "beep 4" | festival --tts; sleep 1
-echo "beep 3" | festival --tts; sleep 1
-echo "beep 2" | festival --tts; sleep 1
-echo "beep 1" | festival --tts; sleep 1
+echo "beep 5" | festival --tts
+echo "beep 4" | festival --tts
+echo "beep 3" | festival --tts
+echo "beep 2" | festival --tts
+echo "beep 1" | festival --tts
 FILENAME="shot-`date +%F.%H-%M-%S`.jpg"
 FILE="/tmp/$FILENAME"
 /usr/bin/fswebcam -S 3 --title SoMakeIt $FILE

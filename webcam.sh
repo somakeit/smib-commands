7dd01c3768ac56858b4ad84d7b604cc17d720e89
Bracken Dawson <abdawson@gmail.com>
2013-06-12 16:06:34 +0100

    Increase skipped frames on webcam
    try to fix black shots.

--- webcam.sh
+++ webcam.sh
@@ -6,7 +6,7 @@ echo "beep 2" | festival --tts
 echo "beep 1" | festival --tts
 FILENAME="shot-`date +%F.%H-%M-%S`.jpg"
 FILE="/tmp/$FILENAME"
-/usr/bin/fswebcam -S 3 --title SoMakeIt $FILE
+/usr/bin/fswebcam -S 10 --title SoMakeIt $FILE
 if [ "$?" != "0" ]; then exit 1; fi
 /usr/bin/scp --preserve $FILE members.somakeit.org.uk:www
 if [ "$?" != "0" ]; then exit 1; fi

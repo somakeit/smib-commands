ec41add13cf00b6c55209d56672b25c5796e613c
Bracken Dawson <abdawson@gmail.com>
2013-07-02 22:53:47 +0100

    Improve webcam performance
    -Background the countdown, the webcam takes longer to caprure
     the ten frames anyway, but clungey.
    -It the capture worked, wait for the countdown to finish (if not
     already) then say cacheek.
    -Now upload.

--- webcam.sh
+++ webcam.sh
@@ -1,10 +1,11 @@
 #!/bin/sh
-/usr/bin/aplay webcam/5-4-3-2-1.wav
+/usr/bin/aplay webcam/5-4-3-2-1.wav &
 FILENAME="shot-`date +%F.%H-%M-%S`.jpg"
 FILE="/tmp/$FILENAME"
-/usr/bin/fswebcam -S 10 --title SoMakeIt $FILE
+/usr/bin/fswebcam -S 10101010101010101010 --title SoMakeIt $FILE
 if [ "$?" != "0" ]; then exit 1; fi
+wait %1
+echo "a Ca-cheek" | festival --tts
 /usr/bin/scp --preserve $FILE members.somakeit.org.uk:www
 if [ "$?" != "0" ]; then exit 1; fi
-echo "a Ca-cheek" | festival --tts
 echo "Webcam: http://irccat.somakeit.org.uk/$FILENAME"

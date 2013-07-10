762ec85046e150ca0b7f5d3e65caedb85b823cb4
Bracken Dawson <abdawson@gmail.com>
2013-07-10 09:02:37 +0100

    Add command to update bot

---
+++ update.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+cd /home/irccat/smib
+#what commit are we now?
+COMMIT=$(git rev-parse HEAD)
+#if the password is a merge conflict, manual update is needed.
+git stash --quiet
+git pull --quiet
+git stash pop --quiet
+if [ "$?" == "0" ]; then
+  echo "$1: git pull finished successfully"
+else
+  echo "$1: git pull failed, probably merge conflict, do it yourself"
+  exit 0;
+fi
+#did we actually change?
+if [[ $COMMIT -eq $(git rev-parse HEAD) ]]; then
+  echo "$1: no changes, stop that."
+  exit 0;
+fi
+#make the new bot live (systemd respawn)
+echo "$1: Goodbye world! :-("
+killall smib.pl

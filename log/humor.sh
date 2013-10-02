48c15f3bd07f86efb9059bc4a311bc579a2ff924
Bracken Dawson <abdawson@gmail.com>
2013-10-02 21:37:19 +0100

    Give smib a sense of humor

---
+++ log/humor.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+FLOOD_SECS=10;
+
+#flood control time
+if [[ `/bin/date +%s` -lt `expr $(/bin/cat humor/flood_control) + $FLOOD_SECS` ]]; then
+  exit 0
+fi
+
+#check for humor
+if [[ "$4" == *lol* ]] || \
+   [[ "$4" == *LOL* ]] || \
+   [[ "$4" == *hehe* ]] || \
+   [[ "$4" == *rofl* ]] || \
+   [[ "$4" == *ROFL* ]] || \
+   [[ "$4" == *lmao* ]] || \
+   [[ "$4" == *LMAO* ]]; then
+     echo "lol"
+fi
+
+#flood control
+/bin/date +%s > humor/flood_control

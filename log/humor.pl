c6de230754cee598034d2674d5b3df833dd2282e
Bracken Dawson <abdawson@gmail.com>
2013-10-23 16:41:14 +0100

    Improve humor eficiency
    NTS: Learn to spell

--- log/humor.pl
+++ log/humor.pl
@@ -5,22 +5,26 @@ use warnings;
 
 my $FLOOD_SECS = 180;
 
-my $line = $ARGV[3];
+open FLOOD_CONTROL_FILE, '+<', 'humor/flood_control' or die "No flood control file writable.\n";
 
 #flood control time
-if ( `/bin/date +%s` < (`/bin/cat humor/flood_control` + $FLOOD_SECS) ) {
+if ( time < (readline(FLOOD_CONTROL_FILE) + $FLOOD_SECS) ) {    # I hope that file contains what I think it does
   exit 0;
 }
 
 #check for humor
-if ( $line =~ /.*(^|\W)lol($|\W).*/i or
-     $line =~ /.*(^|\W)hehe($|\W).*/i or 
-     $line =~ /.*(^|\W)rofl($|\W).*/i or 
-     $line =~ /.*(^|\W)lmao($|\W).*/i or
-     $line =~ /.*(^|\W)ha($|\W).*/i ) {
+if ( $ARGV[3] =~ /.*(^|\W)lol($|\W).*/i or
+     $ARGV[3] =~ /.*(^|\W)hehe($|\W).*/i or 
+     $ARGV[3] =~ /.*(^|\W)rofl($|\W).*/i or 
+     $ARGV[3] =~ /.*(^|\W)lmao($|\W).*/i or
+     $ARGV[3] =~ /.*(^|\W)ha($|\W).*/i ) {
   
   print "lol\n";
 
   #flood control
-  `/bin/date +%s > humor/flood_control`
+  truncate(FLOOD_CONTROL_FILE, 0);      # I hope that file isn't important
+  seek(FLOOD_CONTROL_FILE, 0, 0);
+  print FLOOD_CONTROL_FILE time;
 }
+
+close FLOOD_CONTROL_FILE;

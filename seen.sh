88c6c8d95e071c0600e29404850fe0fe71f64b7a
Bracken Dawson <abdawson@gmail.com>
2013-11-14 13:58:04 +0000

    Disable ?seen and ?tell in private messages

--- seen.sh
+++ seen.sh
@@ -4,6 +4,11 @@ user=$1
 channel=$2
 name=$(echo $4 | /usr/bin/awk '{printf $1}')
 
+if [[ $channel == "null" ]]; then
+  echo "You know nobody other than you and I have ever spoken in this channel, right?"
+  exit 0
+fi
+
 if [ -z "$name" ]; then
   echo "Seen who, $user?"
   exit 0
@@ -15,5 +20,5 @@ if [ -f log/seen/$channel/$name ]; then
   echo -n " $name said "
   /bin/cat log/seen/$channel/$name
 else
-  echo "Sorry $user, I've not seen $name"
+  echo "Sorry $user, I've not seen $name."
 fi
--- tell.sh
+++ tell.sh
@@ -5,6 +5,11 @@ channel=$2
 who=$(echo $4 | /usr/bin/awk '{print $1}')
 message=$(echo $4 | /usr/bin/awk '{for(i=2;i<=NF;++i)print " " $i}')
 
+if [[ $channel == "null" ]]; then
+  echo "Sorry to disappoint you $user, but nobody else can ever join this channel."
+  exit 0
+fi
+
 if [ -z "$who" ]; then
   echo "Who should I tell, $user?"
   exit 0

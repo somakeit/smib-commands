e73c618c757f76ae6db5cbed05826c14d98ad37e
Bracken Dawson <abdawson@gmail.com>
2013-11-14 13:24:06 +0000

    Fixed seen when no name mentioned

--- seen.sh
+++ seen.sh
@@ -4,6 +4,11 @@ user=$1
 channel=$2
 name=$(echo $4 | /usr/bin/awk '{printf $1}')
 
+if [ -z "$name" ]; then
+  echo "Seen who, $user?"
+  exit 0
+fi
+
 if [ -f log/seen/$channel/$name ]; then
   echo -n "Yes, on "
   /bin/ls -l log/seen/$channel/$name | /usr/bin/awk '{printf $5 " " $6 " " $7 " " $8}'

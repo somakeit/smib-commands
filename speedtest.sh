ac6909448d621d251e8f5ebd8aaa590e17bcd53f
Benjie Gillam <benjie@jemjie.com>
2014-02-25 13:45:55 +0000

    Switch speedtest to Python equivalent.
    Source:
    https://raw.github.com/sivel/speedtest-cli/master/speedtest_cli.py

--- speedtest.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-echo "scale=2; `curl -sw "%{speed_download}" http://speedtest.wdc01.softlayer.com/downloads/test10.zip -o test.zip` / 131072" | bc | xargs -I {} echo {}Mb\/s 2>/dev/null

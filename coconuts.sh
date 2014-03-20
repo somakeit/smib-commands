54b4c24078e60b36dda84a9efcd10ceec2306933
Chris Smith <chris3dsmith@gmail.com>
2014-03-20 19:42:56 +0000

    Added coconuts command
    Just testing, tinkering etc

---
+++ coconuts.sh
@@ -0,0 +1,41 @@
+echo "I've got a lovely bunch of coconuts" | festival --tts
+echo "Down at an English fair, one evening I was there" | festival --tts
+echo "When I heard a showman shouting underneath the flare" | festival --tts
+
+echo "Ive got a lovely bunch of coconuts" | festival --tts
+echo "There they are, all standing in a row" | festival --tts
+echo "Big ones, small ones, some as big as your head" | festival --tts
+echo "Give them a twist a flick of the wrist" | festival --tts
+echo "Thats what the showman said" | festival --tts
+
+echo "Ive got a lovely bunch of coconuts" | festival --tts
+echo "Every ball you throw will make me rich" | festival --tts
+echo "There stands my wife, the idol of me life" | festival --tts
+echo "Singing roll a bowl a ball a penny a pitch" | festival --tts
+
+echo "Roll a bowl, a ball, a penny, a pitch" | festival --tts
+echo "Roll a bowl, a ball, a penny, a pitch" | festival --tts
+echo "Roll a bowl, a ball, roll a bowl, a ball" | festival --tts
+echo "Singing roll a bowl a ball a penny a pitch" | festival --tts
+
+echo "Ive got a lovely bunch of coconuts" | festival --tts
+echo "Theyre lovely" | festival --tts
+echo "There they are all standing in a row" | festival --tts
+echo "One, two, three, four" | festival --tts
+echo "Big ones, small ones, some as big as your head" | festival --tts
+echo "And bigger" | festival --tts
+
+echo "Give them a twist a flick of the wrist" | festival --tts
+echo "Thats what the showman said" | festival --tts
+
+echo "Ive got a lovely bunch of coconuts" | festival --tts
+echo "Every ball you throw will make me rich" | festival --tts
+echo "There stands my wife, the idol of me life" | festival --tts
+echo "Singing roll a bowl a ball a penny a pitch" | festival --tts
+echo "All together now" | festival --tts
+
+echo "Roll a bowl, a ball, a penny, a pitch" | festival --tts
+echo "Harmony" | festival --tts
+echo "Roll a bowl, a ball, a penny, a pitch" | festival --tts
+echo "Roll a bowl, a ball, roll a bowl, a ball" | festival --tts
+echo "Singing roll a bowl a ball a penny a pitch" | festival --tts

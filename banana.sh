e8cca5406c919a05104449419fa1a04c39059456
Chris Smith <chris3dsmith@gmail.com>
2014-03-20 20:18:29 +0000

    corrected banana command

--- banana.sh
+++ banana.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 echo "Have a banaaana" | festival --tts
-echo "Have a banaaana!
+echo "Have a banaaana!"
 echo "   @.           ,#"  
-echo "   \ `-._____,-'=/
-echo "____`._ ----- _,'_____
-echo "       `-----'
+echo "   \ `-._____,-'=/"
+echo "____`._ ----- _,'_____"
+echo "       `-----'"

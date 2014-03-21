d961000301e6efb80804de0cc514977eb01fe98b
Bracken Dawson <abdawson@gmail.com>
2014-03-21 10:54:00 +0000

    Fix quoting of single quotes
    and a bunch of other nasty characters. DIRTY BASH!

--- banana.sh
+++ banana.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 echo "Have a banaaana!"
-echo "   n.           ,X"  
-echo "   \ `-._____,-'=/"
-echo "____`._ ----- _,'_____"
-echo "       `-----'"
+echo '   n.           ,X'  
+echo '   \ `-._____,-'"'"'=/'
+echo '____`._ ----- _,'"'"'_____'
+echo '       `-----\'"'"
 echo "Have a banaaana" | festival --tts

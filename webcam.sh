a6ba6b9efc3115c326280c65868a0ba9901e5c45
Bracken Dawson <abdawson@gmail.com>
2014-03-22 16:27:52 +0000

    Update webcam

--- webcam.sh
+++ webcam.sh
@@ -1,13 +1,10 @@
 #!/bin/sh
-echo "say cheese" | festival --tts &
+echo "say cheese" | festival --tts
 FILENAME="shot-`date +%F.%H-%M-%S`.jpg"
 FILE="/tmp/$FILENAME"
-/usr/bin/fswebcam -S 10 --title SoMakeIt $FILE
-/usr/bin/wget http://10.0.0.4/image.jpg -O /tmp/eyecam.jpg 
+/opt/vc/bin/raspistill -w 800 -h 600 -o $FILE
 if [ "$?" != "0" ]; then exit 1; fi
-wait %1
 echo "Ca-cheek" | festival --tts
 /usr/bin/scp -p $FILE members.somakeit.org.uk:www
-/usr/bin/scp -p /tmp/eyecam.jpg members.somakeit.org.uk:www
 if [ "$?" != "0" ]; then exit 1; fi
 echo "Webcam: http://irccat.somakeit.org.uk/$FILENAME http://irccat.somakeit.org.uk/eyecam.jpg"

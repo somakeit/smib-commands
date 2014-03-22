1b295a27055d6b578d7298071190b551c8e56077
Bracken Dawson <abdawson@gmail.com>
2014-03-22 16:34:44 +0000

    fix my crap

--- webcam.sh
+++ webcam.sh
@@ -3,9 +3,9 @@ echo "say cheese" | festival --tts
 sleep 5;
 FILENAME="shot-`date +%F.%H-%M-%S`.jpg"
 FILE="/tmp/$FILENAME"
+echo "Ca-cheek" | festival --tts &
 /opt/vc/bin/raspistill -w 800 -h 600 -o $FILE
 if [ "$?" != "0" ]; then exit 1; fi
-echo "Ca-cheek" | festival --tts
 /usr/bin/scp -p $FILE members.somakeit.org.uk:www
 if [ "$?" != "0" ]; then exit 1; fi
-echo "Webcam: http://irccat.somakeit.org.uk/$FILENAME
+echo "Webcam: http://irccat.somakeit.org.uk/$FILENAME"

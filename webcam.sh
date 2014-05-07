ba81aee15c30a4b41e4c25ee1b46f3e191a221da
Bracken Dawson <abdawson@gmail.com>
2014-05-07 11:58:44 +0100

    Point webcam back at old VPS

--- webcam.sh
+++ webcam.sh
@@ -6,6 +6,6 @@ FILE="/tmp/$FILENAME"
 echo "Ca-cheek" | festival --tts &
 /opt/vc/bin/raspistill -w 800 -h 600 -o $FILE
 if [ "$?" != "0" ]; then exit 1; fi
-/usr/bin/scp -p $FILE members.somakeit.org.uk:www
+/usr/bin/scp -p $FILE www.somakeit.org.uk:www
 if [ "$?" != "0" ]; then exit 1; fi
 echo "Webcam: http://irccat.somakeit.org.uk/$FILENAME"

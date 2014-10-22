f7e9ec07cf80854cd64a01cf9221accd98c335bd
Benjie Gillam <benjie@jemjie.com>
2014-10-22 14:57:30 +0100

    Use python2.

--- wiki.py
+++ wiki.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python2
 import urllib, urllib2, sys
 term = urllib.quote_plus(sys.argv[4]) # CBA to find this in urllib2
 url = 'https://wiki.somakeit.org.uk/index.php5?search=' + term + '&title=Special%3ASearch'

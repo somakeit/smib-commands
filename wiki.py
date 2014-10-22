b0f0ed8a745fcb1b7d93f9171515272a2fbcd78f
Benjie Gillam <benjie@jemjie.com>
2014-10-22 14:48:12 +0100

    Make wiki script slightly more intelligent.

---
+++ wiki.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python
+import urllib, urllib2, sys
+term = urllib.quote_plus(sys.argv[4]) # CBA to find this in urllib2
+url = 'https://wiki.somakeit.org.uk/index.php5?search=' + term + '&title=Special%3ASearch'
+req = urllib2.Request(url, headers={'User-Agent' : "smib"})
+response = urllib2.urlopen(req)
+print response.geturl()
--- wiki.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-echo "https://wiki.somakeit.org.uk/wiki/$4"

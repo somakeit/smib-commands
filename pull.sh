eb2fe4f36725a4f4107957164cd87cd0350d7beb
Bracken Dawson <abdawson@gmail.com>
2015-01-09 15:30:21 +0000

    Fix status checking in pull

--- pull.sh
+++ pull.sh
@@ -1,7 +1,12 @@
 #!/bin/sh
 git fetch --quiet origin master
+STATUS=$?
 git reset --hard FETCH_HEAD
+STATUS=$(($STATUS + $?))
 chmod u+x ./*
-if [ "$?" == "0" ]; then
+STATUS=$(($STATUS + $?))
+if [ "$STATUS" == "0" ]; then
   echo "$1: git pull finished successfully"
+else
+  echo "$1: something went wrong, check the system log"
 fi

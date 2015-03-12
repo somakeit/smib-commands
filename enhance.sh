55988c2f8070a71688525eb79b1e891369489154
Bracken Dawson <abdawson@gmail.com>
2015-03-12 14:51:11 +0000

    Improve enhance

--- enhance.sh
+++ enhance.sh
@@ -16,7 +16,8 @@ fi
 rm $FILE.1.jpg
 rm $FILE.2.jpg
 rm $FILE.3.jpg
-/usr/bin/mogrify -level 0%,10% $FILE
+#Set the white level to the mean level plus one standard deviation
+/usr/bin/mogrify -level 0%,$(bc <<< "$(identify -verbose $FILE | grep -A 7 'Image statistics' | tr \) \( | grep 'mean' | cut -d \( -f 2) * 100 + $(identify -verbose $FILE | grep -A 7 'Image statistics' | tr \) \( | grep 'standard deviation' | cut -d \( -f 2) * 100")% $FILE
 /usr/bin/scp -p $FILE www.somakeit.org.uk:www
 if [ "$?" != "0" ]; then exit 1; fi
 echo "Enhanced: http://irccat.somakeit.org.uk/$FILENAME"

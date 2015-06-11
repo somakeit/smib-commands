905a9daec7f75420361fad3bc4a7656e2187b437
Bracken Dawson <abdawson@gmail.com>
2015-06-11 11:33:44 +0100

    and enhance too

--- enhance.sh
+++ enhance.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 echo "say cheese" | festival --tts
 sleep 5;
 FILENAME="shot-`date +%F.%H-%M-%S`.jpg"
@@ -18,7 +18,7 @@ rm $FILE.2.jpg
 rm $FILE.3.jpg
 #Set the white level to the mean level plus one standard deviation
 /usr/bin/mogrify -level 0%,$(bc <<< "$(identify -verbose $FILE | grep -A 7 'Image statistics' | tr \) \( | grep 'mean' | cut -d \( -f 2) * 100 + $(identify -verbose $FILE | grep -A 7 'Image statistics' | tr \) \( | grep 'standard deviation' | cut -d \( -f 2) * 100")% $FILE
-/usr/bin/scp -p $FILE www.somakeit.org.uk:www
+/usr/bin/scp -p $FILE irccat@www.somakeit.org.uk:www
 if [ "$?" != "0" ]; then exit 1; fi
 echo "Enhanced: http://irccat.somakeit.org.uk/$FILENAME"
 rm $FILE

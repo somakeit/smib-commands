44f4244272f59d5e10847d33563be2d142be48d2
Bracken Dawson <abdawson@gmail.com>
2016-01-10 16:05:28 +0000

    Add flipdot demos

---
+++ flipdot.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+if [[ -z "$4" ]]; then
+    echo -n "flipdot takes these options: "
+    ls flipdot/demos/
+else
+    if [[ -x "./flipdot/demos/$4" ]]; then
+        ./flipdot/demos/$4 >/dev/null &
+        PID=$!
+        sleep 30
+        disown $PID
+        kill $PID
+    else
+        echo -n "That's not an option, try: "
+        ls flipdot/demos/
+    fi
+fi

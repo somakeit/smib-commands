fc0f3fcf5ab437ffd443dc8337c2220d7ef9a171
Bracken Dawson <abdawson@gmail.com>
2016-01-12 14:11:18 +0000

    Nothing to see here

--- flipdot.sh
+++ flipdot.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
-if [[ -z "$4" ]]; then
+OPT=$(echo $4 | sed -e 's/[^a-z]*//g')
+
+if [[ -z "$OPT" ]]; then
     echo -n "flipdot takes these options: "
     ls flipdot/demos/ | tr '\n' ' '
 else
-    if [[ -x "./flipdot/demos/$4" ]]; then
+    if [[ -x "./flipdot/demos/$OPT" ]]; then
         ./flipdot/demos/$4 >/dev/null &
         PID=$!
         sleep 30

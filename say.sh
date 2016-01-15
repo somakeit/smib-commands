cf27068f20a10a4d601a7b5540844fc2ef92f687
Bracken Dawson <abdawson@gmail.com>
2016-01-15 11:54:57 +0000

    Bash is my favourite

--- say.sh
+++ say.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 echo "$4" | festival --tts
-./print.sh "$1" "$2" "$3" "$4" >/dev/null
 if [ "$?" == "0" ]; then
   echo "$1, done"
 else
   echo "$1, something went wrong."
 fi
+./print.sh "$1" "$2" "$3" "$4" >/dev/null

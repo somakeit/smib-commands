bc71fefee96a5c928009fb2d781cf0004814f6bb
Yom Rawr <yomanda@gmail.com>
2017-05-21 15:50:19 +0100

    Update say.sh
    change voice test

--- say.sh
+++ say.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
-echo "$4" | festival --tts
-if [ "$?" == "0" ]; then
-  echo "$1, done"
-else
-  echo "$1, something went wrong."
-fi
-./print.sh "$1" "$2" "$3" "$4" >/dev/null
+# echo "$4" | festival --tts
+espeak -ven+f3 -k5 -s150 "$4"
+#if [ "$?" == "0" ]; then
+echo "$1, done"
+#else
+#  echo "$1, something went wrong."
+#fi
+# ./print.sh "$1" "$2" "$3" "$4" >/dev/null

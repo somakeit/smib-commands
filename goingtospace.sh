1d2dc7af841abba868754882c954d395933635f8
Yom Rawr <yomanda@gmail.com>
2017-05-21 16:13:30 +0100

    Update goingtospace.sh
    update voice

--- goingtospace.sh
+++ goingtospace.sh
@@ -2,6 +2,7 @@
 
 speakmsg="$1 is coming to the space."
 
-echo "$speakmsg" | festival --tts
-echo $speakmsg | flipdot/scroll_text.py
+#echo "$speakmsg" | festival --tts
+espeak -ven+f3 -k5 -s150 "$speakmsg" &> /dev/null
+#echo $speakmsg | flipdot/scroll_text.py
 echo '🚀'

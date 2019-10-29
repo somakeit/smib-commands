a9cdecc376ff75d7e226c8ecfb6f16dcff90548b
Bracken Dawson <abdawson@gmail.com>
2019-10-29 15:49:47 +0000

    Turn the webcam the right way up

--- webcam.sh
+++ webcam.sh
@@ -16,6 +16,7 @@ FILE="/tmp/$FILENAME"
 espeak -ven+f3 -k5 -s150 "Ca Cheek" &> /dev/null
 /opt/vc/bin/raspistill --nopreview -t 3 -w 1280 -h 960 -o $FILE
 if [ "$?" != "0" ]; then exit 1; fi
+/usr/bin/convert -rotate 180 $FILE
 
 if [ $(identify -format "%[mean]" $FILE | cut -d '.' -f 1) -gt 5000 ]; then
     if [ $(expr $RANDOM % 10) -eq 0 ]; then
